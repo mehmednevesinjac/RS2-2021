c8a0fed80fc06ad51766521691edcd4d90347fb1
Mehmed Nevesinjac <mehmed.nevesinjac@edu.fit.ba>
2021-03-14 00:44:49 +0100

    Prve vjezbe is RS 2

---
+++ eProdaja/eProdaja/Controllers/ProizvodController.cs
@@ -0,0 +1,44 @@
+﻿using eProdaja.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eProdaja.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProizvodController : ControllerBase
+    {
+        public IProizvodService _proizvodService { get; set; }
+        public ProizvodController(IProizvodService proizvodService)
+        {
+            _proizvodService = proizvodService;
+        }
+        [HttpGet]
+        public IEnumerable<Proizvod> Get()
+        {
+            return _proizvodService.Get();
+        }
+
+        [HttpGet(template: "{id}")]
+        public Proizvod GetById(int id)
+        {
+            return _proizvodService.GetById(id);
+        }
+
+        [HttpPost]
+        public Proizvod Insert(Proizvod proizvod)
+        {
+            return _proizvodService.Insert(proizvod);
+           
+        }
+
+        [HttpPut(template:"{id}")]
+        public Proizvod Update(int id, Proizvod proizvod)
+        {
+            return _proizvodService.Update(id, proizvod);
+        }
+    }
+}
---
+++ eProdaja/eProdaja/Services/ProizvodService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eProdaja.Services
+{
+    public class ProizvodService : IProizvodService
+    {
+        private static List<Proizvod> _proizvodi;
+
+        static ProizvodService()
+        {
+            _proizvodi = new List<Proizvod>() { 
+                new Proizvod {Id = 1, Name = "Proizvod1"},
+                new Proizvod {Id = 2, Name = "Proizvod2"},
+                new Proizvod {Id = 3, Name = "Proizvod3"}
+        };
+        }
+
+
+        public IEnumerable<Proizvod> Get()
+        {
+            return _proizvodi;
+        }
+
+        public Proizvod GetById(int id)
+        {
+            return _proizvodi.FirstOrDefault(p => p.Id == id);
+        }
+
+        public Proizvod Insert(Proizvod proizvod)
+        {
+            _proizvodi.Add(proizvod); 
+            return proizvod;
+        }
+
+        public Proizvod Update(int id, Proizvod proizvod)
+        {
+            var current = _proizvodi.FirstOrDefault(p => p.Id == id);
+            current.Name = proizvod.Name;
+            return current;
+        }
+    }
+}
